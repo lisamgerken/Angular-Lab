f04403ce4c66901051932389436a9f0557d3d7ff
Lisa Gerken <lisamgerken@yahoo.com>
2017-11-14 22:29:14 -0500

    Angular Lab

---
+++ controller.js
@@ -0,0 +1,17 @@
+(function() {
+	function FormController() {
+		var fc = this;
+		fc.list = [];
+		fc.addList = function(addItem) {
+			fc.list.push(addItem);
+			fc.addItem = "";
+		};
+		fc.removeItem = function(index) {
+			fc.list.splice(index, 1);
+		};
+	}
+
+	angular
+		.module("app")
+		.controller("FormController", FormController)
+})();
